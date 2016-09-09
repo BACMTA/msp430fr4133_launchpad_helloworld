cc69b18888b67271d177fec63f8e9edce87715fc
ondrejh <ondrejh.ck@email.cz>
2016-09-09 13:06:34 +0200

    uart echo

--- main.c
+++ main.c
@@ -54,14 +54,49 @@ void rtc_init(void)
 	RTCCTL = RTCSS__XT1CLK | RTCSR | RTCPS__1024 | RTCIE;
 }
 
+void uart_init(void)
+{
+    // Configure UART pins
+    P1SEL0 |= BIT0 | BIT1;                    // set 2-UART pin as second function
+
+    // Configure UART
+    UCA0CTLW0 |= UCSWRST;                      // Put eUSCI in reset
+    UCA0CTLW0 |= UCSSEL__SMCLK;
+    // Baud Rate calculation
+    UCA0BR0 = 52;                              // 8000000/16/115200 = 4.34
+    UCA0BR1 = 0;
+    UCA0MCTLW = 0x4900 | UCOS16 |  UCBRF_1;   // 8000000/16/115200 - INT(8000000/16/115200)=0.34
+                                              // UCBRSx value = 0x55 (See UG)
+    UCA0CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
+    UCA0IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+}
+
+void pwm_init(void)
+{
+    P1DIR |= BIT6 | BIT7;                      // P1.7 output
+    P1SEL0 |= BIT6 | BIT7;                     // P1.7 options select
+
+    TA0CCR0 = 128;                             // PWM Period/2
+    TA0CCTL1 = OUTMOD_6;                       // TACCR1 toggle/set
+    TA0CCR1 = 32;                              // TACCR1 PWM duty cycle
+    TA0CCTL2 = OUTMOD_6;                       // TACCR2 toggle/set
+    TA0CCR2 = 96;                              // TACCR2 PWM duty cycle
+
+    TA0CTL = TASSEL_1 | MC_3;                  // ACLK, up-down mode
+}
+
 // main program body
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// Stop WDT
 
 	board_init(); // init dco and leds
-	lcd_init();
 	rtc_init();
+	//pwm_init();
+	uart_init();
+	lcd_init();
+
+    PM5CTL0 &= ~LOCKLPM5;
 
     int hour=0;
     int minute=0;
@@ -115,5 +150,24 @@ void __attribute__ ((interrupt(RTC_VECTOR))) RTC_ISR (void)
             __bic_SR_register_on_exit(LPM3_bits);
             break;
         default: break;
+    }
+}
+
+void __attribute__ ((interrupt(USCI_A0_VECTOR))) USCI_A0_ISR (void)
+{
+    //switch(__even_in_range(UCA0IV,USCI_UART_UCTXCPTIFG))
+    switch(UCA0IV)
+    {
+        case USCI_NONE: break;
+        case USCI_UART_UCRXIFG: // receive interrupt
+            while(!(UCA0IFG&UCTXIFG));
+            UCA0IFG &=~ UCRXIFG;            // Clear interrupt
+            UCA0TXBUF = UCA0RXBUF;     // Clear buffer
+            break;
+        case USCI_UART_UCTXIFG: // transmit interrupt
+            UCA0IFG &=~ UCTXIFG;            // Clear interrupt
+            break;
+        case USCI_UART_UCSTTIFG: break;
+        case USCI_UART_UCTXCPTIFG: break;
     }
 }
